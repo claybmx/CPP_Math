24337a4de7b1675f4d80fff8cd444af422810f3f
claybmx <claybom24@gmail.com>
2023-09-27 20:34:30 -0300

    Adicionando a pasta Conjuntos

---
+++ Conjuntos/conjuntos.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace math {
+
+	static void _conjuntos() {
+	}
+	// **************<<_USAGE_>>****************
+	static void _conjuntosUsage() {
+		std::cout << "_conjuntosUsage()...\n";
+	}
+}
---
+++ Conjuntos/main.cpp
@@ -0,0 +1,15 @@
+//#include "vld.h" // No memory leaks DETECTE!
+#include <iostream>
+#include "conjuntos.h"
+#include "utils.h"
+using namespace std;
+
+int main() {
+
+	utils::PrintColour_("<<_CONJUNTOS_>>", BRI);
+
+	math::_conjuntosUsage();
+
+	system("pause");
+	return(0);
+}
