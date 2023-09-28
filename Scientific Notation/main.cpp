1bbaf1924cf59b6c88cb8b4303786414814c3d0d
claybmx <claybom24@gmail.com>
2023-09-27 22:22:59 -0300

    Adicionando a pasta Scientific Notation

---
+++ Scientific Notation/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "utils.h"
+#include "ScientificNumber.h"
+
+int main() {
+
+	utils::PrintColour_("<<_SCIENTIFIC_NOTATION_NUMBERS_>>", FRI);
+
+	system("pause");
+	return(0);
+}
